5e43953179a29d393c6b1c5beb3ab805b6ee09e6
zww <779442053@qq.com>
2020-08-27 10:41:27 +0800

    项目初始化

---
+++ swiftStardy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  swiftStardy
+//
+//  Created by step_zhang on 2020/8/27.
+//  Copyright © 2020 step_zhang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+   //
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         //创建window
+        return true
+    }
+
+   
+
+}
+
